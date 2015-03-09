ea133388a8d42606c50ddf6c342792cbd12f6e89
Benjamin ter Kuile <bterkuile@gmail.com>
2015-03-09 12:28:12 +0100

    generator setup

---
+++ lib/generators/collection_scaffold/collection_scaffold_generator.rb
@@ -0,0 +1,19 @@
+class CollectionScaffoldGenerator < Rails::Generators::NamedBase
+  source_root File.expand_path("../templates", __FILE__)
+  include Rails::Generators::ResourceHelpers
+
+  check_class_collision suffix: "Controller"
+
+  class_option :orm, banner: "NAME", type: :string, required: true,
+                     desc: "ORM to generate the controller for"
+
+  argument :attributes, type: :array, default: [], banner: "field:type field:type"
+
+  def create_route
+    route "collection_resources :#{plural_name}"
+  end
+
+  def create_controller_files
+    template "controller.rb", File.join('app/controllers', controller_class_path, "#{controller_file_name}_controller.rb")
+  end
+end

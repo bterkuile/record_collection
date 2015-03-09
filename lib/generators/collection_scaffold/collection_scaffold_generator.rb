ecb9eef00247428b1f22eca5f21533fa5a428146
Benjamin ter Kuile <bterkuile@gmail.com>
2015-03-09 13:25:43 +0100

    Progress before git clean

--- lib/generators/collection_scaffold/collection_scaffold_generator.rb
+++ lib/generators/collection_scaffold/collection_scaffold_generator.rb
@@ -6,7 +6,6 @@ class CollectionScaffoldGenerator < Rails::Generators::NamedBase
 
   class_option :orm, banner: "NAME", type: :string, required: true,
                      desc: "ORM to generate the controller for"
-
   argument :attributes, type: :array, default: [], banner: "field:type field:type"
 
   def create_route
@@ -15,5 +14,14 @@ class CollectionScaffoldGenerator < Rails::Generators::NamedBase
 
   def create_controller_files
     template "controller.rb", File.join('app/controllers', controller_class_path, "#{controller_file_name}_controller.rb")
+  end
+
+  def create_model_file
+    template 'model.rb', File.join('app/models', class_path, "#{file_name}.rb")
+  end
+
+  protected
+  def parent_class_name
+    options[:parent] || "ActiveRecord::Base"
   end
 end
---
+++ lib/generators/collection_scaffold/templates/model.rb
@@ -0,0 +1,13 @@
+<% module_namespacing do -%>
+class <%= class_name %> < <%= parent_class_name.classify %>
+<% attributes.each do |attribute| -%>
+  # property <%= attribute.name %>, type: <%= attribute.type %>
+<% end -%>
+<% attributes.select(&:reference?).each do |attribute| -%>
+  belongs_to :<%= attribute.name %><%= ', polymorphic: true' if attribute.polymorphic? %><%= ', required: true' if attribute.required? %>
+<% end -%>
+<% if attributes.any?(&:password_digest?) -%>
+  has_secure_password
+<% end -%>
+end
+<% end -%>

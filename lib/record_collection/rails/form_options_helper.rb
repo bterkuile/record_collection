7bd423a455ab9cb526efb0044d7bb43b1fa81aa8
Benjamin ter Kuile <bterkuile@gmail.com>
2015-07-21 13:49:29 +0200

    Refactor for better uniformity

--- lib/record_collection/rails/form_options_helper.rb
+++ lib/record_collection/rails/form_options_helper.rb
@@ -16,15 +16,13 @@ class ActionView::Helpers::FormBuilder
   end
 
   def optional_boolean(attr, options = {})
-    classes = get_optional_classes(attr, options.merge(base_class: 'optional-boolean'))
-    label_tag = label(attr, options[:label])
-    check_box_tag = check_box(attr, options)
-    add_collection_ids @template.content_tag(:div, label_tag + check_box_tag , class: classes, data: get_data_attributes(attr, options))
+    #classes = get_optional_classes(attr, options.merge(base_class: 'optional-boolean'))
+    #label_tag = label(attr, options[:label])
+    #check_box_tag = check_box(attr, options)
+    #add_collection_ids @template.content_tag(:div, label_tag + check_box_tag , class: classes, data: get_data_attributes(attr, options))
+    optional_form_element(:check_box, attr, options.reverse_merge(base_class: 'optional-boolean'))
   end
-
-  def optional_check_box(attr, options = {})
-    optional_boolean(attr, options)
-  end
+  alias_method :optional_check_box, :optional_boolean
 
   def optional_input(attr, options = {})
     classes = get_optional_classes(attr, options.reverse_merge(base_class: 'optional-input'))
@@ -32,42 +30,45 @@ class ActionView::Helpers::FormBuilder
   end
 
   def optional_text_field(attr, options={})
-    classes = get_optional_classes(attr, options.reverse_merge(base_class: 'optional-text-field'))
-    add_collection_ids @template.content_tag(:div, label(attr, options[:label]) + text_field(attr, options), class: classes, data: get_data_attributes(attr))
+    optional_form_element(:text_area, attr, options.reverse_merge(base_class: 'optional-text-field'))
   end
 
   def optional_text_area(attr, options={})
-    classes = get_optional_classes(attr, options.reverse_merge(base_class: 'optional-text-area'))
-    add_collection_ids @template.content_tag(:div, label(attr, options[:label]) + text_area(attr, options), class: classes, data: get_data_attributes(attr))
+    optional_form_element(:text_area, attr, options.reverse_merge(base_class: 'optional-text-area'))
   end
 
+  def get_optional_classes(attr, options = {})
+    classes = [options[:base_class] || 'optional-input', 'optional-attribute-container', attr, active_class(attr)]
+    classes << 'disabled' if options[:disabled]
+    classes << 'one' if !object.is_a?(RecordCollection::Base) or object.one?
+    classes << 'error' if object.errors[attr].present?
+    classes
+  end
+
+  def optional_form_element(element, attr, options = {})
+    classes = get_optional_classes(attr, options.reverse_merge(base_class: 'optional-text-area'))
+    add_collection_ids @template.content_tag(:div, label(attr, options[:label]) + send(element, attr, options), class: classes, data: get_data_attributes(attr, options))
+  end
+
+  private
+
   def get_data_attributes(attr, options = {})
-    one = true
-    one = object.one? if object.is_a? RecordCollection::Base
+    one = object.is_a?(RecordCollection::Base) ? object.one? : true
     attrs = {attribute: attr, one: one}
     attrs[:disabled] = true if options[:disabled]
     attrs
   end
 
-  def get_optional_classes(attr, options = {})
-    active = false
+  def active_class(attr)
+    active = false # default
     if object.respond_to?(:uniform_collection_attribute)
       uniform_value = object.uniform_collection_attribute(attr, set_if_nil: true)
-      # The field is active when the uniform value is not nil
+      # The field is active when the uniform value is not nil, 
+      # aka has a value
       active = !uniform_value.nil?
     end
-    active = true unless object.public_send(attr).nil? # Activate if collection attribute is not nil
-    classes = [options[:base_class] || 'optional-input', 'optional-attribute-container', attr]
-    classes << (active ? 'active' : 'inactive')
-    classes << 'disabled' if options[:disabled]
-    if object.is_a? RecordCollection::Base
-      classes << 'one' if object.one?
-    else
-      # Assume normal record, this always behaves like one
-      classes << 'one'
-    end
-    classes << 'error' if object.errors[attr].present?
-    classes
+    active = true unless object.public_send(attr).nil? # Activate if collection or record attribute is not nil
+    active ? 'active' : 'inactive'
   end
 end
 
--- spec/rails/form_builder_spec.rb
+++ spec/rails/form_builder_spec.rb
@@ -1,8 +1,9 @@
 require 'spec_helper'
 
-RSpec.describe ActionView::Helpers::FormBuilder do
+describe ActionView::Helpers::FormBuilder do
   let(:employee){ Employee.create section: 'SE1' }
-  subject{ described_class.new :collection, Employee::Collection.new([employee]), @template, {}}
+  let(:arguments){ {form_object: Employee::Collection.new([employee])} }
+  subject{ described_class.new :collection, arguments[:form_object], @template, {}}
   before do
     # http://pivotallabs.com/testing-custom-form-builder/
     @template = Object.new
@@ -17,8 +18,79 @@ RSpec.describe ActionView::Helpers::FormBuilder do
     end
 
     it "does not raise when the object is not a collection object" do
-      form_builder = described_class.new :employee, employee, @template, {}
-      expect{ form_builder.collection_ids }.not_to raise_error
+      arguments[:form_object] = employee
+      expect{ subject.collection_ids }.not_to raise_error
     end
+  end
+
+  describe '#get_optional_classes' do
+
+    describe 'active/inactive' do
+      it "does not include active when record has no value" do
+        employee.section = nil
+        subject.get_optional_classes(:section).should_not include'active'
+        subject.get_optional_classes(:section).should include'inactive'
+      end
+
+      it "includes active when record has en empty value" do
+        employee.section = ''
+        subject.get_optional_classes(:section).should include'active'
+        subject.get_optional_classes(:section).should_not include'inactive'
+      end
+
+      it 'includes active when two records have the same value' do
+        employee_2 = Employee.create section: 'SE1'
+        arguments[:form_object] = Employee::Collection.new([employee, employee_2])
+        subject.get_optional_classes(:section).should include'active'
+        subject.get_optional_classes(:section).should_not include'inactive'
+      end
+
+      it 'does not include active when two records have different values' do
+        employee_2 = Employee.create section: 'SE2'
+        arguments[:form_object] = Employee::Collection.new([employee, employee_2])
+        subject.get_optional_classes(:section).should_not include'active'
+        subject.get_optional_classes(:section).should include'inactive'
+      end
+    end
+    
+    describe 'one' do
+      it "includes one if only one record is present" do
+        subject.get_optional_classes(:section).should include'one'
+      end
+
+      it "includes one if the form record is not a collection but a normal record" do
+        arguments[:form_object] = employee
+        subject.get_optional_classes(:section).should include'one'
+      end
+
+      it "does not include one for a collection having more than one records" do
+        employee_2 = Employee.create section: 'SE2'
+        arguments[:form_object] = Employee::Collection.new([employee, employee_2])
+        subject.get_optional_classes(:section).should_not include'one'
+      end
+    end
+
+    describe 'error' do
+      it "has no error class without any arguments set" do
+        subject.get_optional_classes(:section).should_not include'error'
+      end
+
+      it "has no error class with valid argument set" do
+        arguments[:form_object].update(section: 'Se9').should be_truthy
+        subject.get_optional_classes(:section).should_not include'error'
+      end
+
+      it "has error class with invalid argument set" do
+        arguments[:form_object].update(section: 'INVALID NAME').should be false
+        subject.get_optional_classes(:section).should include'error'
+      end
+    end
+
+    describe 'disabled' do
+      it "adds disabled when given as option" do
+        subject.get_optional_classes(:section, disabled: true).should include'disabled'
+      end
+    end
+
   end
 end

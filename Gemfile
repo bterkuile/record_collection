8e78914a9e260b277ee3fca91ca68618810a0042
Benjamin ter Kuile <bterkuile@gmail.com>
2015-07-22 13:24:56 +0200

    Test against multiple rails versions part1

--- Gemfile
+++ Gemfile
@@ -3,6 +3,15 @@ source 'https://rubygems.org'
 # Specify your gem's dependencies in record_collection.gemspec
 gemspec
 
+#http://aaronmiler.com/blog/testing-your-rails-engine-with-multiple-versions-of-rails/
+rails_version = ENV["RAILS_VERSION"] || "default"
+rails = case rails_version
+  when "master" then {github: "rails/rails"}
+  when "default" then ">= 3.2.0"
+  else "~> #{rails_version}"
+end
+gem "rails", rails
+
 group :test do
   gem "codeclimate-test-reporter", require: nil
 end
--- lib/record_collection/rails/form_options_helper.rb
+++ lib/record_collection/rails/form_options_helper.rb
@@ -1,5 +1,4 @@
-require 'action_view/helpers/form_options_helper'
-class ActionView::Helpers::FormBuilder
+ActionView::Helpers::FormBuilder.class_eval do
   # This method adds the collection ids to the form if not already
   # added. For the lazy peaple that forget to add them manually and
   # use an optional tag
@@ -72,3 +71,30 @@ class ActionView::Helpers::FormBuilder
   end
 end
 
+ActionView::Helpers::FormHelper.class_eval do
+  private
+
+  alias_method :old_apply_form_for_options!, :apply_form_for_options!
+  def apply_form_for_options!(record, object, options) #:nodoc:
+    if record.is_a?(RecordCollection::Base)
+        object = convert_to_model(object)
+
+        as = options[:as]
+        namespace = options[:namespace]
+        action, method = [:collection_update, :post]
+        options[:html].reverse_merge!(
+          class:  as ? "#{action}_#{as}" : dom_class(object, action),
+          id:     (as ? [namespace, action, as] : [namespace, dom_id(object, action)]).compact.join("_").presence,
+          method: method
+        )
+
+        options[:url] ||= if options.key?(:format)
+                            polymorphic_path([action, record], format: options.delete(:format))
+                          else
+                            polymorphic_path([action, record], {})
+                          end
+    else
+      old_apply_form_for_options!(record, object, options)
+    end
+  end
+end

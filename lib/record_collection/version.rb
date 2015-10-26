45179f209da792646632a1ed50168255133a5cb8
Benjamin ter Kuile <bterkuile@gmail.com>
2015-10-26 11:16:58 +0100

    Add cursor pointer for non disabled boolean toggles

--- lib/record_collection/version.rb
+++ lib/record_collection/version.rb
@@ -1,3 +1,3 @@
 module RecordCollection
-  VERSION = "0.8.0"
+  VERSION = "0.8.1"
 end
--- spec/dummy/config/routes.rb
+++ spec/dummy/config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
   collection_resources :employees
   collection_resources :projects
+  root to: 'dashboard#home'
 end

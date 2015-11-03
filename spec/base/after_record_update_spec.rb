62cac1bc1cfe5f323bf508e5ea9386d08c71be46
Benjamin <bterkuile@gmail.com>
2015-11-03 18:02:31 +0100

    Better order testing for update hooks

--- spec/base/after_record_update_spec.rb
+++ spec/base/after_record_update_spec.rb
@@ -5,9 +5,17 @@ class AfterRecordUpdateCollection < RecordCollection::Base
 end
 RSpec.describe AfterRecordUpdateCollection do
   subject { described_class.new }
+
   it "executes on the record" do
     employee = Employee.create section: 'SE1', admin: false
     described_class.new([employee]).update({})
+    employee.reload
+    employee.admin.should be true
+  end
+
+  it "will not be overridden by the update action itself (hence after)" do
+    employee = Employee.create section: 'SE1', admin: false
+    described_class.new([employee]).update(admin: false)
     employee.reload
     employee.admin.should be true
   end
--- spec/base/before_record_update_spec.rb
+++ spec/base/before_record_update_spec.rb
@@ -1,7 +1,8 @@
 require 'spec_helper'
 class BeforeRecordUpdateCollection < RecordCollection::Base
   attribute :section
-  before_record_update{|record| record.update(admin: true) }
+  attribute :admin
+  before_record_update{|record| record.admin = true }
 end
 RSpec.describe BeforeRecordUpdateCollection do
   subject { described_class.new }
@@ -10,5 +11,12 @@ RSpec.describe BeforeRecordUpdateCollection do
     described_class.new([employee]).update({})
     employee.reload
     employee.admin.should be true
+  end
+
+  it "will be overriden by the actual update action (hence before)" do
+    employee = Employee.create section: 'SE1', admin: false
+    described_class.new([employee]).update(admin: false)
+    employee.reload
+    employee.admin.should be false
   end
 end
